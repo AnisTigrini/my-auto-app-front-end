bebdf8b7e376c3fd1d77b9780c25b77cab274cb2
AnisTigrini <anistigrini2@gmail.com>
2021-04-15 05:20:40 -0400

    update 9/4/2021

--- src/app/afficher-auto/afficher-auto.component.ts
+++ src/app/afficher-auto/afficher-auto.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { autoService } from '../autoservice';
 
 @Component({
@@ -13,8 +15,13 @@ export class AfficherAutoComponent implements OnInit {
   carrosserie:string[] = ["Camionnette", "VUS", "Berline", "Fourgonnette", 
   "Voitures sport et coupés", "À hayon", "Familiale"];
   autoBrandList:string[] = [];
+  show_images:boolean = false;
+  img_un;
+  img_deux;
+  img_trois;
+
   
-  constructor(private autoService:autoService) { }
+  constructor(private autoService:autoService, private http:HttpClient, private router:Router) { }
 
 
   autForm = new FormGroup({
@@ -22,14 +29,21 @@ export class AfficherAutoComponent implements OnInit {
     prixPost: new FormControl('', [Validators.required]),
     kilometragePost: new FormControl('', [Validators.required]),
     marquePost: new FormControl('', [Validators.required]),
-    versionPost: new FormControl('', [Validators.required]),
-    motricitePost: new FormControl('', [Validators.required]),
+    versionPost: new FormControl('', [Validators.minLength(4)]),
+    motricitePost: new FormControl('', [Validators.minLength(4)]),
     annePost: new FormControl('', [Validators.required]),
     etat: new FormControl('', [Validators.required]),
     carrosseriePost: new FormControl('', [Validators.required]),
     carburantPost: new FormControl('', [Validators.required]),
     transmissionPost: new FormControl('', [Validators.required]),
-    descriptionPost: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    descriptionPost: new FormControl('', [Validators.required, Validators.minLength(20)]),
+    equipement_un: new FormControl('', [Validators.minLength(5)]),
+    equipement_deux: new FormControl('', [Validators.minLength(5)]),
+    equipement_trois: new FormControl('', [Validators.minLength(5)]),
+    equipement_quatre: new FormControl('', [Validators.minLength(5)]),
+    image_un: new FormControl('', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]),
+    image_deux: new FormControl('', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]),
+    image_trois: new FormControl('', [Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]),
   });
 
   ngOnInit(): void {
@@ -43,6 +57,19 @@ export class AfficherAutoComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.autForm.value)
+    this.http.post("http://localhost:5000/api/poster_auto", {'token':localStorage.getItem('token'), 
+    'auto':this.autForm.value}, {'headers': new HttpHeaders().set('content-type', 'application/json')})
+    .subscribe((response) => {
+      if (response["response"] == "success") {
+        this.router.navigate(['/'])
+      }
+    })
+  }
+
+  showImages() {
+    this.show_images = !this.show_images
+    this.img_un = this.autForm.value.image_un
+    this.img_deux = this.autForm.value.image_deux
+    this.img_trois = this.autForm.value.image_trois
   }
 }
--- src/app/profil/mesposts/mesposts.component.ts
+++ src/app/profil/mesposts/mesposts.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MespostsComponent implements OnInit {
 
-  constructor() { }
+  autoList;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.post("http://localhost:5000/api/mes_auto", {'token':localStorage.getItem('token')}, {'headers': new HttpHeaders().set('content-type', 'application/json')})
+    .subscribe((response) => {
+      if (response["reponse"] == "success") {
+        this.autoList = response["autos"]
+        console.log(this.autoList)
+      }
+    })
   }
 
+  handler(refel) {
+    for (var i = 0; i < this.autoList.length; i++) {
+      if (this.autoList[i].idpost == refel.innerHTML) {
+        this.autoList = this.autoList.filter(obj => obj !== this.autoList[i]);
+        this.http.post("http://localhost:5000/api/supprimer_autos", {'token':localStorage.getItem('token'), "idpost":refel.innerHTML}, {'headers': new HttpHeaders().set('content-type', 'application/json')})
+        .subscribe((response) => {
+          console.log(response)
+        })
+      }
+    }
+  }
 }
